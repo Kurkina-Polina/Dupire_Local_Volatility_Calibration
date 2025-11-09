86790a0f5350df16e0ca7deb1042938e9b5e933d
polina <polina@polinas-MacBook-Air.local>
2025-11-09 19:14:30 +0300

    add comment

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from black_scholes import run_black_scholes
 
 
 if __name__ == "__main__":
+    # Вычисление значения стоимости опционов по формуле Блэка-Шоулза и построение трехмерного графика C(Si,ti)
     run_black_scholes()
